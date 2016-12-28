4ba89ed4e0e7dca147886b26596a20df0feb611c
Masayuki Abe <ms.abe@freebit.net>
2016-12-28 18:41:25 +0900

    changed the post time

--- slack_post_co2.py
+++ slack_post_co2.py
@@ -3,22 +3,23 @@
 import sqlite3
 import time
 import requests
-import json 
+import json
 
 
 url = 'your_slack_URL'
-while True:
-    con = sqlite3.connect("../data.db", isolation_level='DEFERRED')
-    cur = con.cursor()
-    cur.execute('SELECT * FROM co2 ORDER BY id desc LIMIT 0, 60;')
-    data = cur.fetchall()
-    con.commit()
-    con.close()
-    data_co2 = []
-    for i in data:
-        data_co2.append(i[2])
-    max_ppm = max(data_co2)
-    if max_ppm >= 2000:
-        payload = {"text": "co2:{}ppm!\nopen the window!".format(max_ppm)}
-        requests.post(url, data=json.dumps(payload))
-    time.sleep(600.0)
+con = sqlite3.connect("../data.db", isolation_level='DEFERRED')
+cur = con.cursor()
+cur.execute('SELECT * FROM co2 ORDER BY id desc LIMIT 0, 0;')
+data = cur.fetchall()
+con.commit()
+con.close()
+data_time = data[0][1]
+data_co2 = data[0][2]
+
+if data_co2 >= 2000:
+    text = "{}\nco2:{}ppm!\nOpen the window!".format(data_time, data_co2)
+elif data_co2 >= 1000:
+    text = "{}\nco2:{}ppm!\nSomeone is there!".format(data_time, data_co2)
+
+payload = {"text": text}
+requests.post(url, data=json.dumps(payload))
